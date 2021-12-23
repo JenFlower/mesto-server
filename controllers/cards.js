985777c9fe4244235b75c425bc3dea66fa25fc99
Olga T <46646722+JenFlower@users.noreply.github.com>
2021-12-24 00:39:52 +0400

    create routers for cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,12 +1,37 @@
 const Card = require('../models/card');
 
 module.exports.createCard = (req, res) => {
-  const { name, link, owner, likes, createdAt } = req.body;
+  const { name, link } = req.body;
 
   // записываем данные в базу
-  Card.create({ name, link, owner, likes, createdAt })
+  Card.create({ name, link })
     // возвращаем записанные в базу данные пользователю
     .then(card => res.send({ data: card }))
     // если данные не записались, вернём ошибку
     .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
 };
+
+module.exports.getCards = (req, res) => {
+  User.find({})
+    .then(cards => res.send({data: cards}))
+    .catch(() => res.status(500).send({message: 'Server error'}));
+};
+
+module.exports.deleteCard = (req, res) => {
+  const { cardId } = req.params;
+  User.findById(cardId)
+    .then(user => res.send({data: user}))
+    .catch(() => res.status(500).send({message: 'Server error'}));
+};
+
+module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
+  req.params.cardId,
+  { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+  { new: true },
+)
+
+module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
+  req.params.cardId,
+  { $pull: { likes: req.user._id } }, // убрать _id из массива
+  { new: true },
+)
